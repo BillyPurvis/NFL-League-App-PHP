32c5dd3ac4da0860a0f556d105463d53d0975db2
Billy Purvis <purvisbilly@outlook.com>
2017-04-14 22:52:12 +0100

    Adds WHERE clause to order by Division & Conference type

--- fetchAll.php
+++ fetchAll.php
@@ -3,8 +3,18 @@
 	require('connection.php');
 
     $orderBy = isset($_GET['q']) ? $_GET['q'] : 'teamPoints';
+    $conferenceOpt = isset($_GET['c']) ? $_GET['c'] : null;
+    $divisionOpt = isset($_GET['d']) ? $_GET['d'] : null;
+
+    $statementAppend = '';
+    if(isset($conferenceOpt)) {
+        $statementAppend = "WHERE teamConference='$conferenceOpt'";
+    } elseif (isset($divisionOpt)) {
+        $statementAppend = "WHERE teamDivision='$divisionOpt'";
+    }
+
 	// statement
-    $statement = "SELECT * FROM nfl_teams ORDER BY $orderBy DESC";
+    $statement = "SELECT * FROM nfl_teams $statementAppend ORDER BY $orderBy DESC";
 	$queryResults = $connection->query($statement);
 	$connection->close();
 	/*
