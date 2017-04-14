06af8a1c7c2d0a9033a6c4304f551a18f86195b3
Billy Purvis <purvisbilly@outlook.com>
2017-04-14 20:30:18 +0100

    changes orderBy value to shorthand if/else

--- fetchAll.php
+++ fetchAll.php
@@ -2,17 +2,11 @@
 	
 	require('connection.php');
 
-	$orderBy = 'teamPoints';
-    if(isset($_GET['q'])) {
-        $orderBy = $_GET['q'];
-    }
+    $orderBy = isset($_GET['q']) ? $_GET['q'] : 'teamPoints';
 	// statement
     $statement = "SELECT * FROM nfl_teams ORDER BY $orderBy DESC";
-
 	$queryResults = $connection->query($statement);
-
 	$connection->close();
-
 	/*
 	 * Before xDebug throws a wobbly with mySQL...
 	 */
