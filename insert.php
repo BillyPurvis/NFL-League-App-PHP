cd9d4c54645db0cf03753aeca16cd8aa106e0980
Billy Purvis <purvisbilly@outlook.com>
2017-04-16 23:33:11 +0100

    Cleans code, builds SQL query (hacky) to avoid long SQL queries in the code and forgetting fields

--- insert.php
+++ insert.php
@@ -6,20 +6,22 @@
 		// Sanitize data
 		$formDataArray = [];
 		foreach ($_POST as $key => $value) {
-			$formDataArray[$key] = htmlspecialchars(mysqli_real_escape_string($connection, $value));	
+			$formDataArray[$key] = htmlspecialchars(mysqli_real_escape_string($connection, $value)) . '\'';
 		}
 	}
+
 	// Fetch Form Data
-    $insertData = [
-        'teamName'      => $formDataArray['teamName'] . '\'',
-        'teamConference'      => $formDataArray['teamConference'] . '\'',
-        'teamPoints'    => $formDataArray['teamPF'] - $formDataArray['teamPA'] . '\'',
-        'teamPF'        => $formDataArray['teamPF'] . '\'',
-        'teamPA'        => $formDataArray['teamPA'] . '\'',
-        'teamWins '	    => $formDataArray['teamWins'] . '\'',
-        'teamLoses'	    => $formDataArray['teamLoses'] . '\'',
-        'teamTies'	    => $formDataArray['teamTies'] . '\'',
-        'teamTDs'	    => $formDataArray['teamTDs'] . '\'',
+	$insertData = [
+        'teamName'       => $formDataArray['teamName'],
+        'teamConference' => $formDataArray['teamConference'],
+        'teamDivision' => $formDataArray['teamDivision'],
+        'teamPoints'    => $formDataArray['teamPF'] - $formDataArray['teamPA'] .' \'',
+        'teamPF'        => $formDataArray['teamPF'],
+        'teamPA'        => $formDataArray['teamPA'],
+        'teamWins '	    => $formDataArray['teamWins'],
+        'teamLoses'	    => $formDataArray['teamLoses'],
+        'teamTies'	    => $formDataArray['teamTies'],
+        'teamTDs'	    => $formDataArray['teamTDs']
     ];
 	// Build Query
 	//$sqlQuery = "INSERT INTO nfl_teams (teamName, teamConference, teamDivision, teamPoints, teamPF, teamPA, teamWins, teamLoses, teamTies, teamTDs) VALUES ('$teamName', '$teamConf', '$teamDivision', $teamPF-$teamPA, $teamPF, $teamPA, $teamWins, $teamLoses, $teamTies, $teamTDs)";
@@ -29,7 +31,10 @@
         '\'' . implode(', \'', array_values($insertData))
     );
 
-	if(!$connection->query($newSql)) {
+    $formattedSQL = rtrim($newSql, '\'');
+
+
+	if(!$connection->query($formattedSQL)) {
 		$_SESSION['error'] = $connection->error;
 	} else {
 		$_SESSION['success'] = "Succesfully added $teamName!";
