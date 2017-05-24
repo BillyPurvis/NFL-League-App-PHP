388725fee2e64bea431df70b8c18e576dc02b6fb
Billy Purvis <purvisbilly@outlook.com>
2017-05-24 23:03:21 +0100

    unlinks team's logo when DELETE request is made

--- delete.php
+++ delete.php
@@ -3,8 +3,12 @@
 require(__DIR__.'/core/bootstrap.php');
 
 $teamID = $_GET['teamID'];
+$teamImage = getSingleTeam($teamID)['teamLogo'];
+
 // delete statement
 $deleteQuery = "DELETE FROM nfl_teams WHERE id=$teamID";
 $connection->query($deleteQuery);
+
+unlink($teamImage);
 
 header('location: /');
