53eecb3b8e930c4b250ac8cfb9474554aeaf0511
Billy Purvis <purvisbilly@outlook.com>
2017-05-25 23:30:32 +0100

    Sets the teamLogo field to empty when deleting a team's image

--- delete-image.php
+++ delete-image.php
@@ -6,4 +6,13 @@ $teamID = $_GET['id'];
 
 removeImage($teamID);
 
+// Set DB field to null if img is deleted
+$statement =  "UPDATE nfl_teams SET teamLogo='' WHERE id='$teamID'";
+
+if(!$connection->query($statement)) {
+    $_SESSION['error'] = $connection->error;
+} else {
+    $_SESSION['success'] = "Successfully updated $teamName!";
+}
+
 header('location: /team/?q='.$teamID);
