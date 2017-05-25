757e92188ac41b7312f3c942d1365f1eee352f01
Billy Purvis <purvisbilly@outlook.com>
2017-05-25 23:34:29 +0100

    Moves DB image path deletion to removeImage function to be reuseable

--- delete-image.php
+++ delete-image.php
@@ -4,15 +4,6 @@ require(__DIR__.'/core/bootstrap.php');
 
 $teamID = $_GET['id'];
 
-removeImage($teamID);
-
-// Set DB field to null if img is deleted
-$statement =  "UPDATE nfl_teams SET teamLogo='' WHERE id='$teamID'";
-
-if(!$connection->query($statement)) {
-    $_SESSION['error'] = $connection->error;
-} else {
-    $_SESSION['success'] = "Successfully updated $teamName!";
-}
+removeImage($teamID, "nfl_teams", "teamLogo");
 
 header('location: /team/?q='.$teamID);
